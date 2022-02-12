27bfc7b4de3503e14f81d71d5221e0367363b0f2
John Bent <johncharlesbent@gmail.com>
2022-02-12 11:46:46 -0500

    revising package.json start script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Thumbnails from "./components/Thumbnails";
 import imagesArr from "./data";
--- src/components/BigImage/index.js
+++ src/components/BigImage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 const BigImage = (props) => {
     console.log(props);
